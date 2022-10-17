a9207a679fa5ae25a1d5163831026e8c8a92eb42
erik-ros <73587189+erik-ros@users.noreply.github.com>
2022-10-17 14:17:51 +0200

    Just a test

--- Statistics.cs
+++ Statistics.cs
@@ -6,6 +6,12 @@
         // public static Maximum(int[] source) : int                            GUSTAV
         // public static Mean(int[] source) : double                            MATTIAS
         // public static Median(int[] source) : double                          ERIK
+
+        //test
+
+
+
+
         // public static Minimum(int[] source) : int                            TOVA
         // public static Mode(int[] source) : int[]                             
         // public static Range(int[] source) : int                              
