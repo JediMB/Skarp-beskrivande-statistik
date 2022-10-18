46ac348f19762a65cd320db2164a49182dc7a0e8
Magisturn <110787093+Magisturn@users.noreply.github.com>
2022-10-18 09:22:28 +0200

    Tillagt StandardDeviation,Maximum,Range

--- Statistics.cs
+++ Statistics.cs
@@ -9,6 +9,29 @@
         // public static Minimum(int[] source) : int                            TOVA
         // public static Mode(int[] source) : int[]                             
         // public static Range(int[] source) : int                              
-        // public static StandardDeviation(int[] source) : double               
+        // public static StandardDeviation(int[] source) : double
+        // 
+
+        public static int Maximum(int[] source)                         //Gustav
+        {
+            int Maximum = source.Max();
+            return Maximum;
+        }
+        public static int Range(int[] source)                           //Gustav
+        {
+            
+            int range = source.Max() - source.Min();
+            return range;
+        }
+
+        public static double StandardDeviation(int[] source)          //Gustav
+        {
+            double savg = 0;
+            double count = source.Count();
+            double standardDeviavrg = source.Average();
+            double standardDevisum = source.Sum(i => (i - standardDeviavrg) * (i - standardDeviavrg));
+            savg = Math.Sqrt(standardDevisum / count);
+            return savg;
+        }
     }
 }
